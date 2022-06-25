473019e156a02ad1351d3fdb6014904191cc7fda
Bestine <bestineokinda@gmail.com>
2022-06-25 11:42:49 +0300

    Added a title label

--- src/grocery_pos/MyFrame.java
+++ src/grocery_pos/MyFrame.java
@@ -1,14 +1,28 @@
 package grocery_pos;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MyFrame extends JFrame{
 	JFrame frame = new JFrame();
 	MyFrame(){
+		JLabel titleLabel = new JLabel();
+		
+		titleLabel.setText("Grocery POS");
+		titleLabel.setBounds(0, 0, 50, 750);
+		titleLabel.setFont(new Font("Mv Boli", Font.BOLD, 50));
+		titleLabel.setForeground(Color.BLUE);
+		
+//		this.setLayout(null);
 		this.setTitle("Grocery POS");
-		this.setSize(750, 420);
+//		this.setSize(750, 420);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(titleLabel);
+		this.pack();
 	}
 
 }
