44a220c20caf915f7cecef970ef36cbbb396bcdd
Bestine <bestineokinda@gmail.com>
2022-06-25 11:00:05 +0300

    Initalized the software

--- src/grocery_pos/MyFrame.java
+++ src/grocery_pos/MyFrame.java
@@ -5,7 +5,10 @@ import javax.swing.JFrame;
 public class MyFrame extends JFrame{
 	JFrame frame = new JFrame();
 	MyFrame(){
-		System.out.println("Hi, Mustafa");
+		this.setTitle("Grocery POS");
+		this.setSize(750, 420);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
