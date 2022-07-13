9c02e686cff0f89e52c240fa454e5b2934c30a48
Bestine <bestineokinda@gmail.com>
2022-07-13 15:16:57 +0300

    Changed all the item labels to item buttons

--- src/grocery_pos/MyFrame.java
+++ src/grocery_pos/MyFrame.java
@@ -18,7 +18,24 @@ import javax.swing.JPanel;
 public class MyFrame extends JFrame implements ActionListener{
 	
 	JFrame frame = new JFrame();
+	
+	
 	JButton mangoButton;
+	JButton appleButton;
+	JButton avocadoButton;
+	JButton bananaButton;
+	JButton coconutButton;
+	JButton orangeButton;
+	JButton pawpawButton;
+	JButton pineappleButton;
+	JButton watermelonButton;
+	
+	JButton cabbageButton;
+	JButton carrotButton;
+	JButton onionButton;
+	JButton pepperButton;
+	JButton spinachButton;
+	JButton tomatoButton;	
 	
 	MyFrame(){
 		//Fruits
@@ -47,15 +64,8 @@ public class MyFrame extends JFrame implements ActionListener{
 		titleLabel.setForeground(Color.green);
 		
 		
-		// create item Label
-		//FRUITS
-//		JLabel mangoLabel = new JLabel();
-//		mangoLabel.setText("Mango");
-//		mangoLabel.setIcon(mangoImage);
-//		mangoLabel.setVerticalTextPosition(JLabel.BOTTOM);
-//		mangoLabel.setHorizontalTextPosition(JLabel.CENTER);
-
-		
+		// create item Buttons
+		//FRUITS		
 		mangoButton = new JButton();
 		mangoButton.addActionListener(this);
 		mangoButton.setText("Mango");
@@ -64,91 +74,118 @@ public class MyFrame extends JFrame implements ActionListener{
 		mangoButton.setHorizontalTextPosition(JButton.CENTER);
 		mangoButton.setFocusable(false);
 		
+		appleButton = new JButton();
+		appleButton.addActionListener(this);
+		appleButton.setText("Apple");
+		appleButton.setIcon(appleImage);
+		appleButton.setVerticalTextPosition(JButton.BOTTOM);
+		appleButton.setHorizontalTextPosition(JButton.CENTER);
+		appleButton.setFocusable(false);
 		
-		JLabel appleLabel = new JLabel();
-		appleLabel.setText("Apple");
-		appleLabel.setIcon(appleImage);
-		appleLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		appleLabel.setHorizontalTextPosition(JLabel.CENTER);
+		avocadoButton = new JButton();
+		avocadoButton.addActionListener(this);
+		avocadoButton.setText("Avocado");
+		avocadoButton.setIcon(avocadoImage);
+		avocadoButton.setVerticalTextPosition(JButton.BOTTOM);
+		avocadoButton.setHorizontalTextPosition(JButton.CENTER);
+		avocadoButton.setFocusable(false);
 		
-		JLabel avocadoLabel = new JLabel();
-		avocadoLabel.setText("Avocado");
-		avocadoLabel.setIcon(avocadoImage);
-		avocadoLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		avocadoLabel.setHorizontalTextPosition(JLabel.CENTER);
+		bananaButton = new JButton();
+		bananaButton.addActionListener(this);
+		bananaButton.setText("Banana");
+		bananaButton.setIcon(bananaImage);
+		bananaButton.setVerticalTextPosition(JButton.BOTTOM);
+		bananaButton.setHorizontalTextPosition(JButton.CENTER);
+		bananaButton.setFocusable(false);
 		
-		JLabel bananaLabel = new JLabel();
-		bananaLabel.setText("Banana");
-		bananaLabel.setIcon(bananaImage);
-		bananaLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		bananaLabel.setHorizontalTextPosition(JLabel.CENTER);
+		coconutButton = new JButton();
+		coconutButton.addActionListener(this);
+		coconutButton.setText("Coconut");
+		coconutButton.setIcon(coconutImage);
+		coconutButton.setVerticalTextPosition(JButton.BOTTOM);
+		coconutButton.setHorizontalTextPosition(JButton.CENTER);
+		coconutButton.setFocusable(false);
 		
-		JLabel coconutLabel = new JLabel();
-		coconutLabel.setText("Coconut");
-		coconutLabel.setIcon(coconutImage);
-		coconutLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		coconutLabel.setHorizontalTextPosition(JLabel.CENTER);
+		orangeButton = new JButton();
+		orangeButton.addActionListener(this);
+		orangeButton.setText("Orange");
+		orangeButton.setIcon(orangeImage);
+		orangeButton.setVerticalTextPosition(JButton.BOTTOM);
+		orangeButton.setHorizontalTextPosition(JButton.CENTER);
+		orangeButton.setFocusable(false);
 		
-		JLabel orangeLabel = new JLabel();
-		orangeLabel.setText("Orange");
-		orangeLabel.setIcon(orangeImage);
-		orangeLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		orangeLabel.setHorizontalTextPosition(JLabel.CENTER);
+		pawpawButton = new JButton();
+		pawpawButton.addActionListener(this);
+		pawpawButton.setText("Pawpaw");
+		pawpawButton.setIcon(pawpawImage);
+		pawpawButton.setVerticalTextPosition(JButton.BOTTOM);
+		pawpawButton.setHorizontalTextPosition(JButton.CENTER);
+		pawpawButton.setFocusable(false);
 		
-		JLabel pawpawLabel = new JLabel();
-		pawpawLabel.setText("Pawpaw");
-		pawpawLabel.setIcon(pawpawImage);
-		pawpawLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		pawpawLabel.setHorizontalTextPosition(JLabel.CENTER);
+		pineappleButton = new JButton();
+		pineappleButton.addActionListener(this);
+		pineappleButton.setText("Pineapple");
+		pineappleButton.setIcon(pineappleImage);
+		pineappleButton.setVerticalTextPosition(JButton.BOTTOM);
+		pineappleButton.setHorizontalTextPosition(JButton.CENTER);
+		pineappleButton.setFocusable(false);
 		
-		JLabel pineappleLabel = new JLabel();
-		pineappleLabel.setText("Pineapple");
-		pineappleLabel.setIcon(pineappleImage);
-		pineappleLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		pineappleLabel.setHorizontalTextPosition(JLabel.CENTER);
-		
-		JLabel watermelonLabel = new JLabel();
-		watermelonLabel.setText("Watermelon");
-		watermelonLabel.setIcon(watermelonImage);
-		watermelonLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		watermelonLabel.setHorizontalTextPosition(JLabel.CENTER);
+		watermelonButton = new JButton();
+		watermelonButton.addActionListener(this);
+		watermelonButton.setText("Watermelon");
+		watermelonButton.setIcon(watermelonImage);
+		watermelonButton.setVerticalTextPosition(JButton.BOTTOM);
+		watermelonButton.setHorizontalTextPosition(JButton.CENTER);
+		watermelonButton.setFocusable(false);
 		
 		//VEGETABLES
-		JLabel cabbageLabel = new JLabel();
-		cabbageLabel.setText("Cabbage");
-		cabbageLabel.setIcon(cabbageImage);
-		cabbageLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		cabbageLabel.setHorizontalTextPosition(JLabel.CENTER);
+		cabbageButton = new JButton();
+		cabbageButton.addActionListener(this);
+		cabbageButton.setText("Cabbage");
+		cabbageButton.setIcon(cabbageImage);
+		cabbageButton.setVerticalTextPosition(JButton.BOTTOM);
+		cabbageButton.setHorizontalTextPosition(JButton.CENTER);
+		cabbageButton.setFocusable(false);
 		
-		JLabel carrotLabel = new JLabel();
-		carrotLabel.setText("Carrot");
-		carrotLabel.setIcon(carrotImage);
-		carrotLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		carrotLabel.setHorizontalTextPosition(JLabel.CENTER);
+		carrotButton = new JButton();
+		carrotButton.addActionListener(this);
+		carrotButton.setText("Carrot");
+		carrotButton.setIcon(carrotImage);
+		carrotButton.setVerticalTextPosition(JButton.BOTTOM);
+		carrotButton.setHorizontalTextPosition(JButton.CENTER);
+		carrotButton.setFocusable(false);
 		
-		JLabel onionLabel = new JLabel();
-		onionLabel.setText("Onion");
-		onionLabel.setIcon(onionImage);
-		onionLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		onionLabel.setHorizontalTextPosition(JLabel.CENTER);
+		onionButton = new JButton();
+		onionButton.addActionListener(this);
+		onionButton.setText("Onion");
+		onionButton.setIcon(onionImage);
+		onionButton.setVerticalTextPosition(JButton.BOTTOM);
+		onionButton.setHorizontalTextPosition(JButton.CENTER);
+		onionButton.setFocusable(false);
 		
-		JLabel pepperLabel = new JLabel();
-		pepperLabel.setText("Pepper");
-		pepperLabel.setIcon(pepperImage);
-		pepperLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		pepperLabel.setHorizontalTextPosition(JLabel.CENTER);
+		pepperButton = new JButton();
+		pepperButton.addActionListener(this);
+		pepperButton.setText("Pepper");
+		pepperButton.setIcon(pepperImage);
+		pepperButton.setVerticalTextPosition(JButton.BOTTOM);
+		pepperButton.setHorizontalTextPosition(JButton.CENTER);
+		pepperButton.setFocusable(false);
 		
-		JLabel spinachLabel = new JLabel();
-		spinachLabel.setText("Spinach");
-		spinachLabel.setIcon(spinachImage);
-		spinachLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		spinachLabel.setHorizontalTextPosition(JLabel.CENTER);
+		spinachButton = new JButton();
+		spinachButton.addActionListener(this);
+		spinachButton.setText("Spinach");
+		spinachButton.setIcon(spinachImage);
+		spinachButton.setVerticalTextPosition(JButton.BOTTOM);
+		spinachButton.setHorizontalTextPosition(JButton.CENTER);
+		spinachButton.setFocusable(false);
 		
-		JLabel tomatoLabel = new JLabel();
-		tomatoLabel.setText("Tomato");
-		tomatoLabel.setIcon(tomatoImage);
-		tomatoLabel.setVerticalTextPosition(JLabel.BOTTOM);
-		tomatoLabel.setHorizontalTextPosition(JLabel.CENTER);
+		tomatoButton = new JButton();
+		tomatoButton.addActionListener(this);
+		tomatoButton.setText("Tomato");
+		tomatoButton.setIcon(tomatoImage);
+		tomatoButton.setVerticalTextPosition(JButton.BOTTOM);
+		tomatoButton.setHorizontalTextPosition(JButton.CENTER);
+		tomatoButton.setFocusable(false);
 		
 		
 		// Add title and items panels
@@ -157,36 +194,36 @@ public class MyFrame extends JFrame implements ActionListener{
 		
 		
 		// Add items to titlePanel
-		titlePanel.setSize(750, 50);
-		titlePanel.setBounds(0, 0, 750, 50);
+		titlePanel.setSize(900, 50);
+		titlePanel.setBounds(0, 0, 900, 50);
 		titlePanel.add(titleLabel);
 		
 		
 		// Add items to the items Panel
-		itemPanel.setSize(750, 550);
-		itemPanel.setBounds(0, 50, 750, 550);
+		itemPanel.setSize(900, 550);
+		itemPanel.setBounds(0, 50, 900, 550);
 		itemPanel.add(mangoButton);
-		itemPanel.add(appleLabel);
-		itemPanel.add(avocadoLabel);
-		itemPanel.add(bananaLabel);
-		itemPanel.add(coconutLabel);
-		itemPanel.add(orangeLabel);
-		itemPanel.add(pineappleLabel);
-		itemPanel.add(pawpawLabel);
-		itemPanel.add(watermelonLabel);
-		itemPanel.add(cabbageLabel);
-		itemPanel.add(carrotLabel);
-		itemPanel.add(onionLabel);
-		itemPanel.add(pepperLabel);
-		itemPanel.add(spinachLabel);
-		itemPanel.add(tomatoLabel);
+		itemPanel.add(appleButton);
+		itemPanel.add(avocadoButton);
+		itemPanel.add(bananaButton);
+		itemPanel.add(coconutButton);
+		itemPanel.add(orangeButton);
+		itemPanel.add(pineappleButton);
+		itemPanel.add(pawpawButton);
+		itemPanel.add(watermelonButton);
+		itemPanel.add(cabbageButton);
+		itemPanel.add(carrotButton);
+		itemPanel.add(onionButton);
+		itemPanel.add(pepperButton);
+		itemPanel.add(spinachButton);
+		itemPanel.add(tomatoButton);
 		
 		
 		this.setLayout(null);
 		this.setResizable(false);
 		this.setTitle("Grocery POS");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(750, 600);
+		this.setSize(900, 600);
 		this.setVisible(true);
 		this.add(titlePanel);
 		this.add(itemPanel);
@@ -198,6 +235,48 @@ public class MyFrame extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		if (e.getSource()==mangoButton) {
 			System.out.println("You just ordered Mango");
+		}
+		else if (e.getSource()==appleButton) {
+			System.out.println("You just ordered Apple");
+		}
+		else if (e.getSource()==avocadoButton) {
+			System.out.println("You just ordered Avocado");
+		}
+		else if (e.getSource()==bananaButton) {
+			System.out.println("You just ordered Banana");
+		}
+		else if (e.getSource()==coconutButton) {
+			System.out.println("You just ordered Coconut");
+		}
+		else if (e.getSource()==orangeButton) {
+			System.out.println("You just ordered Orange");
+		}
+		else if (e.getSource()==pawpawButton) {
+			System.out.println("You just ordered Pawpaw");
+		}
+		else if (e.getSource()==pineappleButton) {
+			System.out.println("You just ordered Pineapple");
+		}
+		else if (e.getSource()==watermelonButton) {
+			System.out.println("You just ordered Watermelon");
+		}
+		else if (e.getSource()==cabbageButton) {
+			System.out.println("You just ordered Cabbage");
+		}
+		else if (e.getSource()==onionButton) {
+			System.out.println("You just ordered Onion");
+		}
+		else if (e.getSource()==pepperButton) {
+			System.out.println("You just ordered Pepper");
+		}
+		else if (e.getSource()==spinachButton) {
+			System.out.println("You just ordered spinach");
+		}
+		else if (e.getSource()==tomatoButton) {
+			System.out.println("You just ordered Tomato");
+		}
+		else if (e.getSource()==watermelonButton) {
+			System.out.println("You just ordered Watermelon");
 		}
 		
 	}
